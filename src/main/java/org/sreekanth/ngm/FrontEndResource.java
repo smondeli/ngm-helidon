29a0f80b4decb85587d493b51b64248d6998fbac
smondeli <sreekanth.mondeli@oracle.com>
2022-12-06 00:49:39 +0530

    Inventory page

--- src/main/java/org/sreekanth/ngm/FrontEndResource.java
+++ src/main/java/org/sreekanth/ngm/FrontEndResource.java
@@ -2,28 +2,72 @@ package org.sreekanth.ngm;
 
 import io.helidon.common.configurable.Resource;
 
+import javax.json.*;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.*;
 
 @Path("/")
 public class FrontEndResource {
+    private static final JsonBuilderFactory JSON = Json.createBuilderFactory(Collections.emptyMap());
 
     @Path("/")
     @GET
     @Produces(MediaType.TEXT_HTML)
     public String home() {
-        String indexFile = Resource.create("web/index.html").string();
-        return indexFile;
+        return Resource.create("web/index.html").string();
     }
 
     @Path("/inventory")
     @GET
     @Produces(MediaType.TEXT_HTML)
-    public String listing() {
-        String indexFile = Resource.create("web/inventory.html").string();
-        return indexFile;
+    public String inventory() {
+        return Resource.create("web/inventory.html").string();
     }
 
+    @Path("/items")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonArray listing() {
+        return JSON.createArrayBuilder()
+                .add(JSON.createObjectBuilder()
+                        .add("name", "Apple")
+                        .add("price", 3)
+                        .add("category", "Fruit")
+                )
+                .add(JSON.createObjectBuilder()
+                        .add("name", "Banana")
+                        .add("price", 2)
+                        .add("category", "Fruit")
+                )
+                .add(JSON.createObjectBuilder()
+                        .add("name", "Peach")
+                        .add("price", 3)
+                        .add("category", "Fruit")
+                )
+                .add(JSON.createObjectBuilder()
+                        .add("name", "Broccoli")
+                        .add("price", 3)
+                        .add("category", "Vegetable")
+                )
+                .add(JSON.createObjectBuilder()
+                        .add("name", "Kale")
+                        .add("price", 5)
+                        .add("category", "Vegetable")
+                )
+                .add(JSON.createObjectBuilder()
+                        .add("name", "Chicken alfredo")
+                        .add("price", 7)
+                        .add("category", "Pasta")
+                )
+                .add(JSON.createObjectBuilder()
+                        .add("name", "Pie")
+                        .add("price", 4)
+                        .add("category", "Dessert")
+                )
+                .build();
+    }
 }
